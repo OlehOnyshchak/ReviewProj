200a81367f62e7813390344b7d24960b5bf1d4b3
RostykStakhiv <rostyk96@gmail.com>
2017-04-26 10:41:58 +0300

    - Created InstitutionDeatilsController; - Started working on InstitutionDetails View (need to add ui for rating and add logic)

---
+++ Project/ReviewProj/Controllers/InstitutionDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReviewProj.Controllers
+{
+    public class InstitutionDetailsController : Controller
+    {
+        // GET: InstitutionDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
