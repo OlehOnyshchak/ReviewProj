fc13a6b4e8906758c55fe70a8189a3a453c8515e
Vova Maryniak <vmaryniak@gmail.com>
2017-04-27 21:54:04 +0300

    Add images from the database to the homepage

--- Project/ReviewProj/Controllers/HomeController.cs
+++ Project/ReviewProj/Controllers/HomeController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ReviewProj.Models;
+using System.IO;
+using System.Drawing;
 
 namespace ReviewProj.Controllers
 {
     public class HomeController : Controller
     {
+
+        ApplicationDbContext context = ApplicationDbContext.Create();
+
         public ActionResult Index()
         {
             //ApplicationUser user = new ApplicationUser { UserName = "check@gmal.com", Email = "check@gmal.com",
@@ -27,7 +32,24 @@ namespace ReviewProj.Controllers
             //    db.SaveChanges();
             //}
 
-            return View(ApplicationDbContext.Create().Enterprises.ToList<Enterprise>());
+
+
+            //byte[] array = null;
+            //using (var ms = new MemoryStream())
+            //{
+            //    Image.FromFile(@"D:\development\ReviewProj\Project\ReviewProj\Content\images\resource_7359.png")
+            //        .Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            //    array = ms.ToArray();
+            //}
+
+            //context.Resources.First().Data = array;
+            //context.SaveChanges();
+
+
+            // Не знаю для чого цей рядок, але з ним все працює.
+            var r = context.Resources.ToList<Resource>();
+
+            return View(context.Enterprises.ToList<Enterprise>());
         }
 
         public ActionResult About()
