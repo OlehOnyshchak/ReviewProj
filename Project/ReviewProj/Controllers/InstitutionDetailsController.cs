2398d58276083c4e081c785e044c91235c776fec
Vova Maryniak <vmaryniak@gmail.com>
2017-04-28 01:21:28 +0300

    Passed model to Institution Details Controller

--- Project/ReviewProj/Controllers/InstitutionDetailsController.cs
+++ Project/ReviewProj/Controllers/InstitutionDetailsController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ReviewProj.Models;
 
 namespace ReviewProj.Controllers
 {
     public class InstitutionDetailsController : Controller
     {
+        ApplicationDbContext context = ApplicationDbContext.Create();
         // GET: InstitutionDetails
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
-            return View();
+            Enterprise ent = context.Enterprises.FirstOrDefault(e => e.EntId == id);
+            return View(ent);
         }
     }
 }
