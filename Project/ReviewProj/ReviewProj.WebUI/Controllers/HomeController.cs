217e3a548110b138ffe78e05a2fc6dfe54b4ecd9
Vova Maryniak <vmaryniak@gmail.com>
2017-05-09 14:57:37 +0300

    Registered admin, owner and reviewer

--- Project/ReviewProj/ReviewProj.WebUI/Controllers/HomeController.cs
+++ Project/ReviewProj/ReviewProj.WebUI/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using ReviewProj.Domain.Concrete;
 using ReviewProj.Domain.Entities;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
 
 namespace ReviewProj.WebUI.Controllers
 {
@@ -19,8 +21,20 @@ namespace ReviewProj.WebUI.Controllers
         }
         public ActionResult Index()
         {
-            IdentityConfig.RegisterAdmins();
             return View(repository.Enterprises);
+        }
+
+
+        // Перевіряємо ролі користувача
+        public ActionResult Roles()
+        {
+            IList<string> roles = new List<string> { "Роль не определена" };
+            ApplicationUserManager userManager = HttpContext.GetOwinContext()
+                                                    .GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
+            if (user != null)
+                roles = userManager.GetRoles(user.Id);
+            return View((object)(roles == null ? "" : roles.First()));
         }
 
         public ActionResult About()
